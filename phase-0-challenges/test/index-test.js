5887f73f59766eab067ca7a036b4be6fa530e25b
alexmwaura <alexmwaura43@gmail.com>
2022-08-04 08:29:09 +0300

    add test case for reverseArray method

--- phase-0-challenges/test/index-test.js
+++ phase-0-challenges/test/index-test.js
@@ -1,9 +1,10 @@
+const { expect } = require("chai");
 const { reverseArray } = require("../index");
 
 describe('Reverse Array', () => {
   describe('Array',  () => {
-    it('returns [9,8,6,5]',async () => {
-    await  expect(reverseArray([5,9,8,6])).toEqual([9,8,6,5])
+    it('returns reversed sorted array', async () => {
+      await expect(reverseArray([5,9,8,6])).toEqual([9,8,6,5])
     })
   })
 })
